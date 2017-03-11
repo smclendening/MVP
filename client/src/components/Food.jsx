5c05d86ba2e13fd5fb5d43cf1fc83e4635026cd5
Scott <smclendening@gmail.com>
2017-03-11 15:48:26 -0800

    Refactor to make food list a table; add basic styling

--- client/src/components/Food.jsx
+++ client/src/components/Food.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 
 const Food = (props) => (
-  <div>
-    <b>{props.date}</b> --- {props.name} --- {props.cals} calories --- {props.comment}
-  </div>
+  <tr>
+    <td>{props.date}</td><td>{props.name}</td><td>{props.cals} calories</td><td>{props.comment}</td>
+  </tr>
 )
 
 export default Food;
--- client/src/components/FoodList.jsx
+++ client/src/components/FoodList.jsx
@@ -4,9 +4,10 @@ import Food from './Food.jsx'
 var date = new Date().toString().slice(0, 10);
 
 const FoodList = (props) => (
-  <div>
+  <table id="food-list">
+    <th>Date</th> <th>Food</th> <th>Calories</th> <th>Comment</th>
     {props.foodList.map( (food) => <Food date={date} name={food.name} cals={food.cals} comment={food.comment}/>)}
-  </div>
+  </table>
 )
 
 export default FoodList;
