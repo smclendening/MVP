4ae10f6e607ec7013cb89b17d780c0ea195e5309
Scott <smclendening@gmail.com>
2017-03-10 20:09:28 -0800

    Mongo database seems to be working - commit before anything else

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,21 +1,31 @@
 var server = require('../server');
 var FoodModel = require('../db');
 var data = [{name: 'Tri-Tip', cals: 50, comment: 'Wow!'}, {name: 'Subway', cals: 500, comment: 'Hmmmm'}];
-
+var Promise = require('Bluebird');
 
 module.exports = {
   'get': function(req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
 
-    FoodModel.find({}, function(err, people) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(people);
-      }
-    });
+    var promise = FoodModel.find({}).exec();
 
-    res.end(JSON.stringify(data));
+    promise.then(function(people) {
+      console.log(people);
+      res.end(JSON.stringify(people));
+    })
+    .catch(function(err) {
+      console.log('error in get request', err);
+    })
+
+    // FoodModel.find({}, function(err, people) {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     console.log(people);
+    //   }
+    // });
+
+    // res.end(people);
   },
   'post': function(req, res) {
     console.log('in post');
@@ -28,11 +38,12 @@ module.exports = {
     req.on('end', () => {
         // we have our body
       var newFood = JSON.parse(body);
-      data.push(newFood);
+      // data.push(newFood);
       var foodEntry = new FoodModel(newFood);
 
       foodEntry.save(function(err) {
         if (err) console.log(err);
+        console.log(foodEntry);
       }); // should be saved now 
       // probably have to JSON parse it 
       res.writeHead(201, {'access-control-allow-origin': '*'});
